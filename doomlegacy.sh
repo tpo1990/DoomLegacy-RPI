9c6bded56e818922ae91b5fe7530e6649c7ec6a4
Thomas Popenda <44737275+tpo1990@users.noreply.github.com>
2019-07-30 11:35:36 +0200

    Add files via upload
    Added Doom registered version, Doom 2, TNT and Plutonia to the scriptmodule.

--- doomlegacy.sh
+++ doomlegacy.sh
@@ -12,7 +12,7 @@
 rp_module_id="doomlegacy"
 rp_module_desc="Doom Legacy - Enhanced splitscreen port of the official DOOM source"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/tpo1990/DoomLegacy-RPI/master/LICENSE"
-rp_module_help="Add your own iwad and pwads to $romdir/ports/doomlegacy"
+rp_module_help="For registered version, add your own iwad and pwads (doom.wad, doom2.wad, tnt.wad and plutonia.wad) to $romdir/ports/doom. Notice: If doom.wad is not found in doom roms folder, shareware version will be installed."
 rp_module_section="exp"
 rp_module_flags=""
 
@@ -33,22 +33,38 @@ function install_doomlegacy() {
 }
 
 function game_data_doomlegacy() {
-    if [[ ! -f "$romdir/ports/doom/doom1.wad" ]]; then
-        wget -nv -O "$romdir/ports/doom/doom1.wad" "$__archive_url/doom1.wad"
-        chown $user:$user "$romdir/ports/doom/doom1.wad"
-    fi
-
     if [[ ! -f "$romdir/ports/doom/legacy.wad" ]]; then
         downloadAndExtract "https://netix.dl.sourceforge.net/project/doomlegacy/1.47.2/doomlegacy_1.47.2_common.zip" "$romdir/ports/doom/"
         mv "$romdir/ports/doom/doomlegacy_1.47.2/legacy.wad" "$romdir/ports/doom/"
         rm -r "$romdir/ports/doom/doomlegacy_1.47.2"
         chown -R $user:$user "$romdir/ports/doom"
     fi
+    
+    if [[ -f "$romdir/ports/doom/doom.wad" ]]; then
+       chown $user:$user "$romdir/ports/doom/doom.wad"
+       addPort "$md_id" "doom" "Doom" "DOOMWADDIR=$romdir/ports/doom $md_inst/bin/doomlegacy -iwad %ROM%" "$romdir/ports/doom/doom.wad"
+    else wget "$__archive_url/doom1.wad" -O "$romdir/ports/doom/doom1.wad"
+       chown $user:$user "$romdir/ports/doom/doom1.wad"
+       addPort "$md_id" "doom" "Doom Shareware" "DOOMWADDIR=$romdir/ports/doom $md_inst/bin/doomlegacy -iwad %ROM%" "$romdir/ports/doom/doom1.wad"
+    fi
+    
+    if [[ -f "$romdir/ports/doom/doom2.wad" ]]; then
+       chown $user:$user "$romdir/ports/doom/doom2.wad"
+       addPort "$md_id" "doom" "Doom II: Hell on Earth" "DOOMWADDIR=$romdir/ports/doom $md_inst/bin/doomlegacy -iwad %ROM%" "$romdir/ports/doom/doom2.wad"
+    fi
+    
+    if [[ -f "$romdir/ports/doom/tnt.wad" ]]; then
+       chown $user:$user "$romdir/ports/doom/tnt.wad"
+       addPort "$md_id" "doom" "Final Doom - TNT: Evilution" "DOOMWADDIR=$romdir/ports/doom $md_inst/bin/doomlegacy -iwad %ROM%" "$romdir/ports/doom/tnt.wad"
+    fi
+    
+    if [[ -f "$romdir/ports/doom/plutonia.wad" ]]; then
+       chown $user:$user "$romdir/ports/doom/plutonia.wad"
+       addPort "$md_id" "doom" "Final Doom - The Plutonia Experiment" "DOOMWADDIR=$romdir/ports/doom $md_inst/bin/doomlegacy -iwad %ROM%" "$romdir/ports/doom/plutonia.wad"
+    fi
 }
 
 function configure_doomlegacy() {
-    addPort "$md_id" "doom" "Doom" "DOOMWADDIR=$romdir/ports/doom $md_inst/bin/doomlegacy -iwad %ROM%" "$romdir/ports/doom/doom1.wad"
-
     mkRomDir "ports/doom"
     mkRomDir "$md_inst/bin/wads"
 
